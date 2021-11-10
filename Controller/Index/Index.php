b46d35afb3fd5ba138a0b89a92c07f0b0bb46179
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2021-11-10 13:39:34 +0200

    Frontend action to validate recaptcha.

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,11 +1,26 @@
 <?php
 namespace Swissup\RecaptchaElavonConvergePayment\Controller\Index;
 
-use \Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Action\Context;
 
 class Index extends Action
 {
+    private $helper;
+
+    /**
+     * @param \Magento\Captcha\Helper\Data $helper
+     * @param Context                     $context
+     */
+    public function __construct(
+        \Magento\Captcha\Helper\Data $helper,
+        Context $context
+    ) {
+        $this->helper = $helper;
+        parent::__construct($context);
+    }
+
     /**
      * @inheritdoc
      */
@@ -13,7 +28,43 @@ class Index extends Action
     {
         /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultRedirectFactory->create();
+        $formId = $this->getRequest()->getParam('form_id', '');
+        $token = $this->getRequest()->getParam('token', '');
+        $captcha = $formId ? $this->helper->getCaptcha($formId) : null;
+        $response = $captcha ? $captcha->verify($token) : null;
+        $isSuccess = $response && $response->isSuccess();
 
-        return $resultRedirect->setPath('*/*/');
+        if ($isSuccess) {
+            if ($this->getRequest()->isAjax()) {
+                return $this->resultFactory
+                    ->create(ResultFactory::TYPE_JSON)
+                    ->setData([
+                        'status' => 'ok'
+                    ]);
+            } else {
+                $this->messageManager->addSuccessMessage(__("Recaptcha is valid"));
+
+                return $resultRedirect->setPath('');
+            }
+        }
+
+        $errors = implode(
+            $response ? $response->getErrorCodes() : ['missing-form-id'],
+            ', '
+        );
+        if ($this->getRequest()->isAjax()) {
+            return $this->resultFactory
+                    ->create(ResultFactory::TYPE_JSON)
+                    ->setData([
+                        'status' => 'error',
+                        'message' => __("Invalid recaptcha. [error-code: %1]", $errors)
+                    ]);
+        }
+
+        $this->messageManager->addErrorMessage(
+            __("Invalid recaptcha. [error-code: %1]", $errors)
+        );
+
+        return $resultRedirect->setPath('');
     }
 }
